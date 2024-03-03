7c373545d66a6c9c8173f0381ffe050c815b098b
Wagner Cardoso <wagnerdev01@gmail.com>
2024-03-03 00:19:54 -0300

    wip: Add display input handling

--- src/components/display/index.tsx
+++ src/components/display/index.tsx
@@ -1,10 +1,20 @@
+import { useState, FormEvent } from 'react';
 import { DisplayActions } from './actions';
 
 export function Display() {
+  const [calc, setCalc] = useState<string>('');
+
+  const onChangeCalc = (event: FormEvent<HTMLInputElement>) =>
+    setCalc(event.currentTarget.value);
+
   return (
     <div className="flex items-center justify-between h-[76px] border-t-[1px] border-gray-600">
       <DisplayActions />
-      <span className="text-[36px] font-medium">57</span>
+      <input
+        className="text-[36px] font-medium flex-1 w-full bg-transparent text-right focus:outline-none"
+        value={calc}
+        onChange={onChangeCalc}
+      />
     </div>
   );
 }
